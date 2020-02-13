65f38cf55970c2bab1d9cefd88764612aba0e7c9
Herman van Dompseler <herman@dompseler.nl>
2020-02-13 12:20:28 +0100

    typo

--- startup.sh
+++ startup.sh
@@ -4,8 +4,8 @@ cd /home/node/app
 
 # copy secrets
 mkdir src/controllers/config
-cp $KEY_PRIV src/controllers/config/jwtRS256.key
-cp $KEY_PUB src/controllers/config/jwtRS256.key.json
+cp $KEY_PRIV_FILE src/controllers/config/jwtRS256.key
+cp $KEY_PUB_FILE src/controllers/config/jwtRS256.key.json
 
 # install dep & go
 npm install
